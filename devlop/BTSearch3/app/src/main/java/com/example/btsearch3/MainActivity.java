817c87884b13eb27ca82cc2d88afb6a884966ab2
nakashima shinya <nakasin52@gmail.com>
2018-02-09 13:34:42 +0900

    Fix

--- devlop/BTSearch3/app/src/main/java/com/example/btsearch3/MainActivity.java
+++ devlop/BTSearch3/app/src/main/java/com/example/btsearch3/MainActivity.java
@@ -9,7 +9,14 @@ import android.bluetooth.le.ScanResult;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+//import java.util.ArrayList;
+//import java.util.List;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static android.content.ContentValues.TAG;
 
@@ -18,11 +25,17 @@ public class MainActivity extends Activity {
     private BluetoothManager bleManager;
     private BluetoothAdapter bleAdapter;
     private BluetoothLeScanner bleScanner;
+    ListView listView;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+        adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1);
+        list = new ArrayList<>();
 
         // Bluetoothの使用準備.
         bleManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
@@ -47,16 +60,21 @@ public class MainActivity extends Activity {
             super.onScanResult(callbackType, result);
             Log.d(TAG,"call onScanSucceed");
 
-            if (result.getDevice().getName() != null){
-                setContentView(R.layout.activity_main);
-                ((TextView)findViewById(R.id.textView)).setText("Name:" + result.getDevice().getName());
-                ((TextView)findViewById(R.id.textView2)).setText("Address:" + result.getDevice().getAddress());
-                ((TextView)findViewById(R.id.textView3)).setText("BluetoothType:" + result.getDevice().getType());
-                ((TextView)findViewById(R.id.textView4)).setText("Uuids:" + result.getDevice().getUuids());
-                ((TextView)findViewById(R.id.textView5)).setText("BondState:" + result.getDevice().getBondState());
+            if (result.getDevice().getName() != null && !list.contains(result.getDevice().getName())){
+                adapter.add(result.getDevice().getName());
+                list.add(result.getDevice().getName());
+
+//                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_main,);
+//                ((TextView)findViewById(R.id.textView)).setText("Name:" + result.getDevice().getName());
+//                ((TextView)findViewById(R.id.textView2)).setText("Address:" + result.getDevice().getAddress());
+//                ((TextView)findViewById(R.id.textView3)).setText("BluetoothType:" + result.getDevice().getType());
+//                ((TextView)findViewById(R.id.textView4)).setText("Uuids:" + result.getDevice().getUuids());
+//                ((TextView)findViewById(R.id.textView5)).setText("BondState:" + result.getDevice().getBondState());
+
 
             }
-
+            listView.setAdapter(adapter);
+            scanNewDevice();
         }
 
         @Override
