cc49abf99a0397b684f322446621d0b8fd0cd76f
nakashima shinya <nakasin52@gmail.com>
2018-02-13 16:24:59 +0900

    コンパイル内容修正

--- devlop/BTSearch3/app/src/main/java/com/example/btsearch3/MainActivity.java
+++ devlop/BTSearch3/app/src/main/java/com/example/btsearch3/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.btsearch3;
 
+import android.Manifest;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
@@ -7,16 +8,13 @@ import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-//import java.util.ArrayList;
-//import java.util.List;
-
 import java.util.ArrayList;
-import java.util.List;
 
 import static android.content.ContentValues.TAG;
 
@@ -29,10 +27,15 @@ public class MainActivity extends Activity {
     ArrayAdapter<String> adapter;
     ArrayList<String> list;
 
+    static final int REQUEST_CODE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         listView = findViewById(R.id.listView);
         adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1);
         list = new ArrayList<>();
@@ -41,7 +44,14 @@ public class MainActivity extends Activity {
         bleManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         bleAdapter = bleManager.getAdapter();
 
-        if ((bleAdapter != null) || (bleAdapter.isEnabled())) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M)
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                //許可を求めるダイアログを表示します。
+                //TODO ダイアログがでない。
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            }
+
+        if ((bleAdapter != null) && (bleAdapter.isEnabled())) {
             // BLEが使用可能ならスキャン開始.
             this.scanNewDevice();
         }
